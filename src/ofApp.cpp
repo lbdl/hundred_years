b373507ebd14a741a330ed561221a634d2704347
Tim Storey <tstorey@gameaccount.com>
2021-05-15 21:05:59 +0100

    Make isosphere.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,15 +3,124 @@
 //--------------------------------------------------------------
 void ofApp::setup() {
 
+    ofSetVerticalSync(true);
+    ofBackground(20);
+    float width	 = ofGetWidth() * .12;
+    float height	= ofGetHeight() * .12;
+
+    icoSphere.setRadius( width );
+
+
+    ofSetSmoothLighting(true);
+
+    pointLight.setDiffuseColor( ofFloatColor(.85, .85, .55) );
+    pointLight.setSpecularColor( ofFloatColor(1.f, 1.f, 1.f));
+
+    pointLight2.setDiffuseColor( ofFloatColor( 238.f/255.f, 57.f/255.f, 135.f/255.f ));
+    pointLight2.setSpecularColor(ofFloatColor(.8f, .8f, .9f));
+
+    pointLight3.setDiffuseColor( ofFloatColor(19.f/255.f,94.f/255.f,77.f/255.f) );
+    pointLight3.setSpecularColor( ofFloatColor(18.f/255.f,150.f/255.f,135.f/255.f) );
+
+    // shininess is a value between 0 - 128, 128 being the most shiny //
+    material.setShininess( 120 );
+    // the light highlight of the material //
+    material.setSpecularColor(ofColor(255, 255, 255, 255));
+
 }
 //--------------------------------------------------------------
 void ofApp::update(){
+    pointLight.setPosition((ofGetWidth()*.5)+ cos(ofGetElapsedTimef()*.5)*(ofGetWidth()*.3), ofGetHeight()/2, 500);
+    pointLight2.setPosition((ofGetWidth()*.5)+ cos(ofGetElapsedTimef()*.15)*(ofGetWidth()*.3),
+                            ofGetHeight()*.5 + sin(ofGetElapsedTimef()*.7)*(ofGetHeight()), -300);
 
+    pointLight3.setPosition(
+            cos(ofGetElapsedTimef()*1.5) * ofGetWidth()*.5,
+            sin(ofGetElapsedTimef()*1.5f) * ofGetWidth()*.5,
+            cos(ofGetElapsedTimef()*.2) * ofGetWidth()
+            );
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    float spinX = sin(ofGetElapsedTimef()*.35f);
+    float spinY = cos(ofGetElapsedTimef()*.075f);
 
+//    if (bMousePressed) {
+//        spinX = spinY = 0.0f;
+//    }
+
+    cam.setGlobalPosition({ 0,0,cam.getImagePlaneDistance(ofGetCurrentViewport()) });
+    cam.begin();
+
+    ofEnableDepthTest();
+
+    ofEnableLighting();
+    pointLight.enable();
+    pointLight2.enable();
+    pointLight3.enable();
+
+
+
+    // draw the outer sphere
+    material.begin();
+    ofNoFill();
+    ofDrawSphere(0, 0, max(ofGetWidth(),ofGetHeight()));
+    material.end();
+
+    float screenWidth = ofGetWidth();
+    float screenHeight = ofGetHeight();
+
+    icoSphere.setPosition( -screenWidth * .5 + screenWidth *  1/4.f, screenHeight * -1.1/6.f, 0);
+
+    // ICO Sphere //
+    icoSphere.rotateDeg(spinX, 1.0, 0.0, 0.0);
+    icoSphere.rotateDeg(spinY, 0, 1.0, 0.0);
+
+//    if (mode == 3) {
+//        triangles = icoSphere.getMesh().getUniqueFaces();
+//    }
+
+    bool bFill = true;
+    if (bFill) {
+        material.begin();
+        ofFill();
+
+        //mode == 3
+        if (true) {
+            float angle = (ofGetElapsedTimef() * 1.4);
+            glm::vec3 faceNormal;
+            for (size_t i = 0; i < triangles.size(); i++) {
+                float frc = ofSignedNoise(angle* (float)i * .1, angle*.05) * 4;
+                faceNormal = triangles[i].getFaceNormal();
+                for (int j = 0; j < 3; j++) {
+                    triangles[i].setVertex(j, triangles[i].getVertex(j) + faceNormal * frc);
+                }
+            }
+            icoSphere.getMesh().setFromTriangles(triangles);
+        }
+
+        icoSphere.draw();
+        material.end();
+    }
+    bool bWireframe = true;
+    if (bWireframe) {
+        ofNoFill();
+        ofSetColor(0, 0, 0);
+        icoSphere.setScale(1.01f);
+        icoSphere.drawWireframe();
+        icoSphere.setScale(1.f);
+    }
+
+    ofDisableDepthTest();
+
+    ofFill();
+
+    cam.end();
+
+    ofSetDrawBitmapMode(OF_BITMAPMODE_MODEL_BILLBOARD);
+
+    ofDrawBitmapStringHighlight("ofIcoSpherePrimitive", glm::vec3(-90,+160,0) + cam.worldToScreen( icoSphere.getPosition()));
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,22 +3,43 @@
 #include "ofMain.h"
 #include "ofxGui.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+public:
+    void setup();
+
+    void update();
+
+    void draw();
+
+    void keyPressed(int key);
+
+    void keyReleased(int key);
+
+    void mouseMoved(int x, int y);
+
+    void mouseDragged(int x, int y, int button);
+
+    void mousePressed(int x, int y, int button);
+
+    void mouseReleased(int x, int y, int button);
+
+    void mouseEntered(int x, int y);
+
+    void mouseExited(int x, int y);
+
+    void windowResized(int w, int h);
+
+    void dragEvent(ofDragInfo dragInfo);
+
+    void gotMessage(ofMessage msg);
+
+    ofCamera cam;
+    ofLight pointLight;
+    ofLight pointLight2;
+    ofLight pointLight3;
+    ofMaterial material;
+    ofIcoSpherePrimitive icoSphere;
+    vector<ofMeshFace> triangles;
 };
